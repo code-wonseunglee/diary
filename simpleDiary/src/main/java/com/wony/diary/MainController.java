b7e5cf40aff0a80dff48ebb890374534f40331a0
이승원 <81894632+code-wonseunglee@users.noreply.github.com>
2021-07-21 09:35:28 +0900

    기초 설정

---
+++ simpleDiary/src/main/java/com/wony/diary/MainController.java
@@ -0,0 +1,23 @@
+package com.wony.diary;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/test")
+	public ModelAndView test() {
+		
+		ModelAndView mv = new ModelAndView("/test");
+		
+		return mv;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ simpleDiary/src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>톰켓 연뎔되는지 테스트용</title>
+</head>
+<body>
+	테스트입니다.
+</body>
+</html>
