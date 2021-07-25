55546be26b3ddc5142c91f0ba5f6b3cc3edc3e50
이승원 <81894632+code-wonseunglee@users.noreply.github.com>
2021-07-25 22:30:32 +0900

    Simple

---
+++ simpleDiary/src/main/java/com/wony/diary/service/DiaryDAO.java
@@ -0,0 +1,19 @@
+package com.wony.diary.service;
+
+import java.util.List;
+
+import com.wony.diary.dto.DiaryWriteDTO;
+
+public interface DiaryDAO {
+	
+	public void write(DiaryWriteDTO dto) throws Exception;
+	
+	public DiaryDAO read(Integer bno) throws Exception;
+	
+	public void update(DiaryWriteDTO dto) throws Exception;
+
+	public void delete(Integer bno) throws Exception;
+	
+	public List<DiaryWriteDTO> listAll() throws Exception;
+	
+}
